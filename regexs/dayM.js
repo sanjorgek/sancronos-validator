62ece6cbe34e0aeaef0d3f48cfd5cfe838e74ab0
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-07-11 19:35:53 -0500

    added missing pattern

--- regexs/dayM.js
+++ regexs/dayM.js
@@ -2,6 +2,6 @@ const alpha = "((3[01])|([12]?[0-9]))";
 
 const beta = "([*]((\/"+alpha+")?))";
 
-const gamma = "("+alpha+"((-"+alpha+"((\/"+alpha+")?))?))";
+const gamma = "("+alpha+"(((-"+alpha+")?)((\/"+alpha+")?)))";
 
 module.exports = "((("+alpha+"|(L)?)[W])|(L)|(("+gamma+"|"+beta+")(([,]("+gamma+"|"+beta+"))*)))";
